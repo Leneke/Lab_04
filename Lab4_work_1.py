0e94d13ff089f71182b6aadfbd2e385418d0aca8
Leneke <farfunchik@gmail.com>
2023-04-13 16:27:37 +0600

    improvements made

--- Lab4_work_1.py
+++ Lab4_work_1.py
@@ -1,5 +1,5 @@
 # Task №1. Create a class and set up logging in two different folders
-from datetime import datetime
+from datetime import datetime, time, timedelta
 from abc import ABC, abstractmethod
 import logging.handlers
 import my_logger
@@ -19,7 +19,7 @@ class AbstractCustomsHouse(ABC):
         pass
 
     @abstractmethod
-    def see_date(self, border_crossing_date):
+    def see_date(self, start_date, end_date):
         pass
 
 
@@ -49,18 +49,22 @@ class CustomsAir(AbstractCustomsHouse):
         for i in self.registration_list:
             print(i)
 
-    def see_date(self, border_crossing_date: int):
-        """The method prints information about those who crossed the border on a specific date
+    def see_date(self, start_date: str, end_date:str):
+        """The method displays information about those who crossed the border for a certain period
         Options:
-                border_crossing_date: int - date of border crossing
+                start_date: str start date in "dd-mm-yyyy" format,
+                end_date: str end date in "dd-mm-yyyy" format.
         Return value:
                 str - number of border crossings on a given date"""
-        logging.info(f'Called up a list of those who crossed the air border on the {border_crossing_date}')
+        logging.info(f'Viewed list of air border crossings for the period from {start_date} to {end_date}')
+        start_period = datetime.strptime(start_date, format('%d.%m.%Y'))
+        end_period = datetime.strptime(end_date, format('%d.%m.%Y'))
         count_number_of_crossings = 0
         for i in self.registration_list:
-            if i['Date'] == border_crossing_date:
+            entry_date = datetime.strptime(i['Date'], '%d.%m.%Y')
+            if start_period <= entry_date <= end_period:
                 count_number_of_crossings += 1
-        return f'Air border crossing date: {border_crossing_date} \n' \
+        return f'Air border crossing period: from {start_date} to {end_date} \n' \
                f'Number of border crossings: {count_number_of_crossings}'
 
 
@@ -90,20 +94,23 @@ class CustomsLand(AbstractCustomsHouse):
         for i in self.registration_list:
             print(i)
 
-    def see_date(self, border_crossing_date: int):
-        """The method prints information about those who crossed the land border on a specific date
+    def see_date(self, start_date: str, end_date: str):
+        """The method displays information about those who crossed the border for a certain period
         Options:
-                border_crossing_date: int - date of border crossing
+                start_date: str start date in "dd-mm-yyyy" format,
+                end_date: str end date in "dd-mm-yyyy" format.
         Return value:
-                str - number of land border crossings on a given date"""
-        logging.info(f'Called up a list of those who crossed the land border on the {border_crossing_date}')
-        count_of_border_crossing_date = 0
+                str - number of border crossings on a given date"""
+        logging.info(f'Viewed list of air border crossings for the period from {start_date} to {end_date}')
+        start_period = datetime.strptime(start_date, format('%d.%m.%Y'))
+        end_period = datetime.strptime(end_date, format('%d.%m.%Y'))
+        count_number_of_crossings = 0
         for i in self.registration_list:
-            if i['Date'] == border_crossing_date:
-                count_of_border_crossing_date += 1
-        return f'Land border crossing date: {border_crossing_date} \n' \
-               f'Number of border crossings: {count_of_border_crossing_date}'
-
+            entry_date = datetime.strptime(i['Date'], '%d.%m.%Y')
+            if start_period <= entry_date <= end_period:
+                count_number_of_crossings += 1
+        return f'Land border crossing period: from {start_date} to {end_date} \n' \
+               f'Number of border crossings: {count_number_of_crossings}'
 
 def example_land():
     # An example of creating an instance of a class CustomsLand and using class methods
@@ -115,7 +122,7 @@ def example_land():
     horgos.register_list_add('Dasha')
     print(horgos.display_register_list())
     print()
-    print(horgos.see_date('01.04.2023'))
+    print(horgos.see_date('02.04.2023', '04.04.2023'))
     print()
 
 
@@ -127,7 +134,7 @@ def example_air():
     almaty.register_list_add('Diana')
     print(almaty.display_register_list())
     print()
-    print(almaty.see_date('02.04.2023'))
+    print(almaty.see_date('02.04.2023', '14.04.2023'))
 
 
 if __name__ == '__main__':
--- my_logger.py
+++ my_logger.py
@@ -6,20 +6,24 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-# Setting up logging to the main folder in the current directory
-new_path_folder = Path(pathlib.Path.cwd(), "folder_log")
-new_path_folder.mkdir(parents=True, exist_ok=True)
-new_path_file = Path(new_path_folder, "logcode.log")
-new_path_file.touch(exist_ok=True)
-logger_handler = logging.handlers.RotatingFileHandler(new_path_file, maxBytes=5000, backupCount=2)
-logger.addHandler(logger_handler)
-logger_handler.setFormatter(logger_formatter)
 
-# Setting up logging to a backup folder in home directory
+def path_create_logfile(path_folder, path_file):
+    """Function to create a path to save the log file. It takes two parameters as input:
+    1. Folder creation path and 2. File creation path."""
+    path_folder.mkdir(parents=True, exist_ok=True)
+    path_file.touch(exist_ok=True)
+    logger_handler = logging.handlers.RotatingFileHandler(path_file, maxBytes=5000, backupCount=2)
+    logger.addHandler(logger_handler)
+    logger_handler.setFormatter(logger_formatter)
+
+
+# Path to the main folder in the current directory
+new_path_folder = Path(pathlib.Path.cwd(), "folder_log")
+new_path_file = Path(new_path_folder, "logcode.log")
+
+# Path to back up folder in home directory
 new_path_folder_copy = Path(pathlib.Path.home(), "folder_log_copy")
-new_path_folder_copy.mkdir(parents=True, exist_ok=True)
 new_path_file_copy = Path(new_path_folder_copy, "logcode_copy.log")
-new_path_file_copy.touch(exist_ok=True)
-logger_handler = logging.handlers.RotatingFileHandler(new_path_file_copy, maxBytes=5000, backupCount=2)
-logger.addHandler(logger_handler)
-logger_handler.setFormatter(logger_formatter)
+
+path_create_logfile(new_path_folder, new_path_file)
+path_create_logfile(new_path_folder_copy, new_path_file_copy)
